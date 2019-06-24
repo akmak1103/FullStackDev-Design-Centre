52601616d31c5edba7d8a3ba148dc58cd5256626
Akshay Makkar <akshaym1103@gmail.com>
2019-06-24 08:10:39 +0530

    Base Project

---
+++ Full Stack/Assets/Scripts/databse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class databse : MonoBehaviour
+{
+    public GameObject website,code,button;
+    void OnMouseDown()
+    {
+        website.SetActive(true);
+        code.SetActive(false);
+        button.SetActive(false);
+    }
+}
---
+++ Full Stack/Assets/Scripts/websiteClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class websiteClick : MonoBehaviour
+{
+    public GameObject code,db;
+    public GameObject layers;
+    void OnMouseDown()
+    {
+        code.SetActive(true);
+        db.SetActive(true);
+        Animator anim = layers.GetComponent<Animator>();
+        anim.SetInteger("click",1)
+;    }
+}
